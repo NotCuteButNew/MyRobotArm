36e343b4a5070f09209c814b77599dbd2d429fe2
WeiDong Bu <bwd2386884119@gmail.com>
2023-05-18 09:29:35 +0800

    修改格式，添加了SDSS的pin值

--- pinout.h
+++ pinout.h
@@ -1,44 +1,45 @@
 #ifndef PINOUT_H_
 #define PINOUT_H_
 
-//pinout of RAMPS 1.4
+// pinout of RAMPS 1.4
 
-//RAMPS 1.4 PINS
-#define X_STEP_PIN         54
-#define X_DIR_PIN          55
-#define X_ENABLE_PIN       38
-#define X_MIN_PIN           3
-#define X_MAX_PIN           2
- 
-#define Y_STEP_PIN         60
-#define Y_DIR_PIN          61
-#define Y_ENABLE_PIN       56
-#define Y_MIN_PIN          14
-#define Y_MAX_PIN          15
+// RAMPS 1.4 PINS
+#define X_STEP_PIN 54
+#define X_DIR_PIN 55
+#define X_ENABLE_PIN 38
+#define X_MIN_PIN 3
+#define X_MAX_PIN 2
 
-#define Z_STEP_PIN         46
-#define Z_DIR_PIN          48
-#define Z_ENABLE_PIN       62
-#define Z_MIN_PIN          18
-#define Z_MAX_PIN          19
+#define Y_STEP_PIN 60
+#define Y_DIR_PIN 61
+#define Y_ENABLE_PIN 56
+#define Y_MIN_PIN 14
+#define Y_MAX_PIN 15
 
-#define E0_STEP_PIN        26
-#define E0_DIR_PIN         28
-#define E0_ENABLE_PIN      24
-#define E0_MIN_PIN         20
+#define Z_STEP_PIN 46
+#define Z_DIR_PIN 48
+#define Z_ENABLE_PIN 62
+#define Z_MIN_PIN 18
+#define Z_MAX_PIN 19
 
-#define E1_STEP_PIN        36
-#define E1_DIR_PIN         34
-#define E1_ENABLE_PIN      30
+#define E0_STEP_PIN 26
+#define E0_DIR_PIN 28
+#define E0_ENABLE_PIN 24
+#define E0_MIN_PIN 20
 
-#define BYJ_PIN_0          40
-#define BYJ_PIN_1          63
-#define BYJ_PIN_2          59
-#define BYJ_PIN_3          64
+#define E1_STEP_PIN 36
+#define E1_DIR_PIN 34
+#define E1_ENABLE_PIN 30
 
-#define SERVO_PIN           4
+#define BYJ_PIN_0 40
+#define BYJ_PIN_1 63
+#define BYJ_PIN_2 59
+#define BYJ_PIN_3 64
 
-#define LED_PIN            13
+#define SERVO_PIN 4
 
+#define LED_PIN 13
+
+#define SDSS 53
 
 #endif
